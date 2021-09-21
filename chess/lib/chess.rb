dca44310278f2d697000063337ff1b2b3d3f12a9
ASkauen <andreas@rubynor.com>
2021-09-21 08:41:38 +0200

    Add methods for updating fen string

--- chess/lib/chess.rb
+++ chess/lib/chess.rb
@@ -6,12 +6,9 @@ class Chess
   def initialize
     @board = EMPTY_BOARD
     @fen = STARTING_FEN
-    print_board
-    parse_fen
-    print_board
   end
 
-  def parse_fen
+  def update_board
     r = 1
     @fen[:placement].split("/").each do |row|
       c = 1
@@ -27,14 +24,46 @@ class Chess
     end
   end
 
-  def print_board
+  def update_turn
+    @fen[:turn] = @fen[:turn] == "w" ? "b" : "w"
+  end
+
+  def update_full_move
+    @fen[:full_move] += 1
+  end
+
+  def update_position
+    out = []
+    split_to_rows.each_value do |row|
+      blank = 0
+      section = []
+      row.each_value do |p|
+        if [W_SQUARE, B_SQUARE].include?(p)
+          blank += 1
+        else
+          section << blank if blank > 0
+          section << PIECES.key(p)
+          blank = 0
+        end
+      end
+      section << blank if blank > 0
+      out << section.join("")
+    end
+    @fen[:position] = out.join("/")
+  end
+
+  def split_to_rows
     out = {}
-    puts ''
     @board.each do |xy, val|
       out[xy[0]] = out[xy[0]].nil? ? {} : out[xy[0]]
       out[xy[0]].store(xy, val)
     end
-    puts out.map { |row| (row[1].map { |xy, val| val }).join(' ') }
+    out
+  end
+
+  def print_board
+    puts ''
+    puts split_to_rows.map { |row| (row[1].map { |xy, val| val }).join(' ') }
     puts ''
   end
 end
