4b27d56af1270054d52958e49e5650b3214c65ea
ASkauen <andreas@rubynor.com>
2021-09-27 14:57:07 +0200

    Create method for getting and displaying legal moves

--- chess/lib/pieces.rb
+++ chess/lib/pieces.rb
@@ -6,13 +6,43 @@ require_relative './board'
 class Piece
   include Resources
   attr_accessor :position
-  attr_reader :icon
+  attr_reader :icon, :id
 
-  def initialize(id, position, board)
+  def initialize(id, position, board, offsets)
     @id = id
     @position = position
     @board = board
+    @offsets = offsets
     @icon = ICONS[id.to_sym]
+    @offsets[0][1] = -1 if @id == 'p'
+  end
+
+  def legal_moves
+    legal = []
+    if 'pkn'.include?(@id.downcase)
+      @offsets.each do |offset|
+        s_check = [(@position[0] + offset[0]), (@position[1] + offset[1])]
+        legal << s_check if [W_SQUARE, B_SQUARE].include?(@board.board[s_check])
+      end
+    else
+      @offsets.each do |offset|
+        s_check = [(@position[0] + offset[0]), (@position[1] + offset[1])]
+        while [W_SQUARE, B_SQUARE].include?(@board.board[s_check])
+          legal << s_check
+          s_check = [(s_check[0] + offset[0]), (s_check[1] + offset[1])]
+        end
+      end
+    end
+    legal
+  end
+
+  def show_moves
+    legal_moves.each do |s|
+      @board.board[s] = W_SQUARE.red
+    end
+    @board.print_board
+    @board.board = @board.empty_board
+    @board.update_board
   end
 
   def move(to); end
@@ -20,81 +50,105 @@ end
 
 class King < Piece
   def initialize(id, position, board)
-    super(id, position, board)
-    @move_offsets = [
-      [+1, 0],
-      [+1, -1],
-      [+1, +1],
-      [0, +1],
-      [0, -1],
-      [-1, 0],
-      [-1, -1],
-      [-1, +1]
-    ]
+    super(
+      id,
+      position,
+      board,
+      [
+        [+1, 0],
+        [+1, -1],
+        [+1, +1],
+        [0, +1],
+        [0, -1],
+        [-1, 0],
+        [-1, -1],
+        [-1, +1]
+      ]
+    )
   end
 end
 
 class Queen < Piece
   def initialize(id, position, board)
-    super(id, position, board)
-    @move_offsets = [
-      [+1, 0],
-      [+1, -1],
-      [+1, +1],
-      [0, +1],
-      [0, -1],
-      [-1, 0],
-      [-1, -1],
-      [-1, +1]
-    ]
+    super(
+      id,
+      position,
+      board,
+      [
+        [+1, 0],
+        [+1, -1],
+        [+1, +1],
+        [0, +1],
+        [0, -1],
+        [-1, 0],
+        [-1, -1],
+        [-1, +1]
+      ]
+    )
   end
 end
 
 class Rook < Piece
   def initialize(id, position, board)
-    super(id, position, board)
-    @move_offsets = [
-      [+1, 0],
-      [0, +1],
-      [0, -1],
-      [-1, 0]
-    ]
+    super(
+      id,
+      position,
+      board,
+      [
+        [+1, 0],
+        [0, +1],
+        [0, -1],
+        [-1, 0]
+      ]
+    )
   end
 end
 
 class Bishop < Piece
   def initialize(id, position, board)
-    super(id, position, board)
-    @move_offsets = [
-      [+1, -1],
-      [+1, +1],
-      [-1, -1],
-      [-1, +1]
-    ]
+    super(
+      id,
+      position,
+      board,
+      [
+        [+1, -1],
+        [+1, +1],
+        [-1, -1],
+        [-1, +1]
+      ]
+    )
   end
 end
 
 class Knight < Piece
   def initialize(id, position, board)
-    super(id, position, board)
-    @move_offsets = [
-      [+2, -1],
-      [+2, +1],
-      [-2, -1],
-      [-2, +1],
-      [+1, -2],
-      [+1, +2],
-      [-1, -2],
-      [-1, +2]
-    ]
+    super(
+      id,
+      position,
+      board,
+      [
+        [+2, -1],
+        [+2, +1],
+        [-2, -1],
+        [-2, +1],
+        [+1, -2],
+        [+1, +2],
+        [-1, -2],
+        [-1, +2]
+      ]
+    )
   end
 end
 
 class Pawn < Piece
   def initialize(id, position, board)
-    super(id, position, board)
-    @move_offsets = [
-      [0, +1]
-    ]
+    super(
+      id,
+      position,
+      board,
+      [
+        [0, +1]
+      ]
+    )
   end
 end
