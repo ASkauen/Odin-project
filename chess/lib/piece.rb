92786a6ab5fe4be571e5219de154e7083c10c541
ASkauen <andreas@rubynor.com>
2021-10-11 15:32:19 +0200

    Fix illegal move functions and add castling

--- chess/lib/piece.rb
+++ chess/lib/piece.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class Piece
   include Resources
-  attr_accessor :position
-  attr_reader :icon, :id, :color, :has_moved
+  attr_accessor :position, :has_moved
+  attr_reader :icon, :id, :color
 
   def initialize(id, position, board, child)
     @id = id
@@ -14,7 +14,9 @@ class Piece
   end
 
   def show_moves
-    legal_moves.each do |s|
+    moves = legal_moves
+    moves -= pin_moves(moves)
+    moves.each do |s|
       @board.board[s] = @board.board[s].red
     end
     @board.print_board
@@ -35,13 +37,18 @@ class Piece
     enemy_pieces = @color == 'white' ? @board.black_pieces : @board.white_pieces
     moves.each do |move|
       temp = @position
+      piece_at_move_square = @board.get_piece(move)
+      @board.active_pieces.delete(@board.get_piece(move))
       @position = move
       @board.update_board
+      @board.update_vars
       enemy_moves = []
       enemy_pieces.each {|p| p.legal_moves.each {|m| enemy_moves << m}}
-      pinned << move if enemy_moves.include?(king.position)
+      pinned << move if king.in_check?
       @position = temp
+      @board.active_pieces << piece_at_move_square if piece_at_move_square
       @board.update_board
+      @board.update_vars
     end
     pinned
   end
@@ -50,25 +57,32 @@ class Piece
     moves = legal_moves
     moves -= pin_moves(moves)
     if moves.include?(to)
+      castle(to) if @id.downcase   == 'k' && legal_castle_moves.include?(to)
       ep_capture(to) if to == @board.fen.en_passant && @id.downcase == 'p'
       @board.active_pieces.delete(@board.get_piece(to))
+      ep_square = get_ep_square(to)
       @position = to
       @has_moved = true
       promotion if @id.downcase == 'p'
       @board.update_board
       @board.print_board
-      @board.fen.update_all(get_ep_square(to))
-      puts 'Check' if checking?
+      @board.fen.update_all(ep_square)
     else
       puts "Illegal move #{to}"
       false
     end
   end
 
-  def double_move(to)
-    return true if @id.downcase == 'p' && ((@position[1] - to[1]).abs > 1)
+  def castle(to)
+    rook_row = to[0] == 3 ? 1 : 8
+    rook = @board.get_piece([rook_row, to[1]])
+    new_rook_pos = rook_row == 1 ? [4, to[1]] : [6, to[1]]
+    rook.position = new_rook_pos
+    rook.has_moved = true
+  end
 
-    false
+  def double_move(to)
+    (@position[1] - to[1]).abs > 1
   end
 
   def ep_capture(behind)
