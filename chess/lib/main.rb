36a9269db50e9f51bc7784259b6c25fb22105612
ASkauen <andreas@rubynor.com>
2021-10-12 14:46:46 +0200

    Remove testing lines

--- chess/lib/main.rb
+++ chess/lib/main.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
 require_relative './board'
-require_relative './resources'
-require 'pry'
-include Resources
 
 game = Board.new
-
-# game.fen.placement = "rnbqkbnr/ppp1pppp/8/8/8/8/PPP1PPPP/R3KBNR"
-game.active_pieces = game.create_pieces
-game.update_board
-game.update_vars
-game.print_board
-
-game.play
+game.new_or_load
