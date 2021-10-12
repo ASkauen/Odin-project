2e5cc69ca4a3a428ea1da1c1fd4a36a98a2de8df
ASkauen <andreas@rubynor.com>
2021-10-12 08:20:02 +0200

    Fix gsub string

--- chess/lib/fen.rb
+++ chess/lib/fen.rb
@@ -81,11 +81,12 @@ class Fen
   end
 
   def update_castling
-    @castling = @castling.gsub('QK', '') if @board.white_king.has_moved
+    @castling = @castling.gsub('KQ', '') if @board.white_king.has_moved
     @castling = @castling.gsub('Q', '') if @QR.has_moved
     @castling = @castling.gsub('K', '') if @KR.has_moved
-    @castling = @castling.gsub('qk', '') if @board.black_king.has_moved
+    @castling = @castling.gsub('kq', '') if @board.black_king.has_moved
     @castling = @castling.gsub('q', '') if @qr.has_moved
     @castling = @castling.gsub('k', '') if @kr.has_moved
+
   end
 end
